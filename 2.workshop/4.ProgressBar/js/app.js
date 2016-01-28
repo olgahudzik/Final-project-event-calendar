ba816495040667a0541ae5e559542f464e47c850
Agata Malec-Sromek <malec.agata@gmail.com>
2016-01-28 13:56:29 +0100

    update ProgressBar workshop

--- 2.workshop/4.ProgressBar/js/app.js
+++ 2.workshop/4.ProgressBar/js/app.js
@@ -1,23 +1,3 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
-
-$(document).ready( function(){
-    var buttons = $(".changeBar");
-    var allProgressBars = $(".progress-bar");
-
-    buttons.on("click", function(event){
-        var id = $(this).data("id");
-        var color = $(this).data("color");
-        var width = $(this).data("width");
-
-        var myProgressBar = allProgressBars.filter("#bar" + id);
-        var mySpan = myProgressBar.find("span");
-
-        mySpan.removeClass();
-        mySpan.addClass(color);
-        mySpan.animate({
-            width : width
-        }, 1000);
-    });
-});

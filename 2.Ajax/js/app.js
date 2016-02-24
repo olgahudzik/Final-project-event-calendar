0abf2a0cffbe2eae51e72ea7c7dc4d7959a2b1ff
agatablue <agatablue@gmail.com>
2016-02-24 21:00:37 +0100

    update Ajax

---
+++ 2.Ajax/js/app.js
@@ -0,0 +1,56 @@
+$(function() {
+
+  //variables for main ul list
+  var movieLists = '';
+  //variables for url
+  var movieUrl = '';
+
+
+  /* Insert Movies to DOM
+  */
+  function insertContent(data) {
+    $.each(data, function(key, value) {
+
+    });
+  }
+
+  /* Edit Content: Set attribute contenteditable to true for every element to update
+  Show button .updateMovie
+  */
+  function editContent() {
+
+  }
+
+  /* Update Content: Set attribute contenteditable to false.
+  Prepare data to update, send to server
+  */
+  function updateContent() {
+
+  }
+
+  /* Remove Content: Get id element to remove
+  */
+  function removeContent() {
+
+  }
+
+
+  /* Load movies and insert them into the DOM
+  */
+  function loadMovies() {
+
+  }
+
+  /* Add new movie to collection. Get all need information and send it to the serwer
+  */
+  function addMovie() {
+
+  }
+
+  loadMovies();
+  editContent();
+  updateContent();
+  removeContent();
+  addMovie();
+
+});

e6231db3229ea264c1d95aa165d8b46108714c99
Olga Hudzik <hudzik.olga@gmail.com>
2017-03-26 13:59:35 +0200

    projekt kalendarz 26.03

--- calendar.js
+++ calendar.js
@@ -9,6 +9,8 @@ $(function(){
 
 console.log(prev,next,monthLabel, allMonths);
 
+
+//Funkcja sprawiajaca, ze po nacisnieciu w event wyskakuje formularz do rejestracji
   eventItem.on('click', function(){
 
       var newDiv = $("<div class='newDiv'><form class='registerForm'><h1 class='formHeadline'>Express Your Interest!</h1><center>Fill in the form below to express you interest in this event. You will be automatically added to its standby list.</center><br><div class='inputsGroup'><label>First Name</label><input type='text'><br><label>Last Name</label><input type='text'><br><label>Email</label><input type='email'><br><input type='submit' value='Count me in' class='formSubmit'><div></form><br><br><button class='close'>X Close</button></div>");
@@ -26,16 +28,43 @@ console.log(prev,next,monthLabel, allMonths);
 
     });
 
-  next.on('click', function(){
-    jQuery.each( allMonths, function( i, val ) {
-        i++;
-        monthLabel.text(val + ' 2017');
-	     });
-  });
-  prev.on('click', function(){
-    jQuery.each( allMonths, function( i, val ) {
-        i--;
-        monthLabel.text(val + ' 2017');
-	     });
-  });
+  //Funkcja sluzaca przesuwaniu miesiecy kalendarza
+    var April = $('#calendarApril');
+    var May = $('#calendarMay');
+    var June = $('#calendarJune');
+    i = 3;
+    next.on('click', function(){
+      i++;
+      monthLabel.text(allMonths[i] + ' 2017');
+
+      if (allMonths[i]=="May"){
+        May.removeClass("nonDisplay")
+      } else if (allMonths[i]=="April"){
+        April.removeClass("nonDisplay");
+      } else if (allMonths[i]=="June") {
+        June.removeClass("nonDisplay");
+      }
+    });
+    prev.on('click', function(){
+      i--;
+      monthLabel.text(allMonths[i] + ' 2017');
+
+      if (allMonths[i]=="May"){
+        May.toggleClass("display");
+        April.addClass("nonDisplay");
+        June.addClass("nonDisplay");
+      } else if (allMonths[i]=="April"){
+        April.toggleClass("display");
+        June.addClass("nonDisplay");
+        May.addClass("nonDisplay");
+      } else if (allMonths[i]=="June") {
+        June.toggleClass("display");
+        May.addClass("nonDisplay");
+        April.addClass("nonDisplay");
+      }
+    });
+
+//Wyszukiwanie eventow w gornej sekcji kalendarza
+
+
 });
